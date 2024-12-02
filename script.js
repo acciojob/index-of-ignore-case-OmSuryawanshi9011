eec9b637fcfac487e993f9d98a1d4dbd6e6a51c2
OmSuryawanshi9011 <131766249+OmSuryawanshi9011@users.noreply.github.com>
2024-12-03 04:39:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function indexOfIgnoreCase(s1, s2) {
-  // write your code here
+  // convert both strings to the same case
+  var lowerS1 = s1.toLowerCase();
+  var lowerS2 = s2.toLowerCase();
+
+  // find the index of the first occurrence of s2 in s1
+  return lowerS1.indexOf(lowerS2);
 }
 
 // Please do not change the code below
